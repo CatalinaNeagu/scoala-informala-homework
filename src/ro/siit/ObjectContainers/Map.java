c39d84aa7b48d1a26caa7c6cfb439b4c4022cde2
CatalinaNeagu <catalina.moiseanu@yahoo.com>
2019-07-29 20:38:10 +0300

    Week6

---
+++ src/ro/siit/ObjectContainers/Map.java
@@ -0,0 +1,14 @@
+package ro.siit.ObjectContainers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+
+public class Map {
+    public static void HashMap() {
+
+
+
+
+    }
+}
---
+++ src/ro/siit/ObjectContainers/Somer.java
@@ -0,0 +1,7 @@
+package ro.siit.ObjectContainers;
+
+public class Somer extends Person {
+    public Somer(String name, int age) {
+        super(name, age);
+    }
+}
